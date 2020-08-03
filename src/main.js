5f206f9fd82cc22386014234bbc040fd487807e6
mmj <mmj@ssb.no>
2020-08-03 09:56:45 +0200

    Forgot to export all components

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-export { InfoPopup, InfoText } from './components'
+export { ErrorMessage, InfoPopup, InfoText } from './components'
 export { SSB_COLORS, SSB_STYLE } from './configurations'
 export { LANGUAGE, REACT_TABLE_TEXT } from './enums'
 export { getLocalizedGsimObjectText, getNestedObject, reactTableCustomFilterMethod, truncateString } from './utilities'
